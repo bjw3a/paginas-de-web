83637b3ff0dca814e141159fa08b444e2d6b44de
Brennan Walsh <56426721+bjw3a@users.noreply.github.com>
2019-12-12 15:08:01 -0600

    Add files via upload

---
+++ newscript.js
@@ -0,0 +1,32 @@
+function runCode(){
+    alert("hey hello there");
+}
+ function runDick(){
+     
+    var name = prompt('are you cool'); if(name=='yes') {alert('then you can come to my party'); } else {
+        alert('Then you cannot come to my party')
+    }
+    
+ } 
+ 
+ 
+ 
+function goDaddy(){
+    var x = prompt('how old are you?'); if(x > 40) { alert('you are an old bastard');}
+    else { alert('you are a young gun baby');}
+}
+
+function runCode2(){
+    var pill=prompt('where do you live?'); {alert("that's tight bro!");}
+}
+function helloSir2(){
+    var x=prompt('por favor dime tu nombre');
+    alert("meow meow meow meow");
+}
+
+function bigBoy(){
+    var blue = prompt("what is your favorite color"); if(blue=='blue') {
+        alert('you have chosen correctly sir! or mam!');} 
+        else{ alert('that is a dumb color and I think you should re think your choice');}
+    
+}
